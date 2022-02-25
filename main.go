94812d0f6cc09a162c0bbbc61666c1ba0b8ed6fe
Bhanu Kiran Chaluvadi <nthdimensionalreality@gmail.com>
2022-02-25 17:23:00 +0100

    Added Uploading and Downloading files using http mutli part form data content type

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/pluralsight/inventoryservice/database"
 	"github.com/pluralsight/inventoryservice/product"
+	"github.com/pluralsight/inventoryservice/receipt"
 )
 
 const apiBasePath = "/api"
@@ -14,5 +15,6 @@ const apiBasePath = "/api"
 func main() {
 	database.SetupDatabase()
 	product.SetupRoutes(apiBasePath)
+	receipt.SetupRoutes(apiBasePath)
 	log.Fatal(http.ListenAndServe(":5123", nil))
 }
